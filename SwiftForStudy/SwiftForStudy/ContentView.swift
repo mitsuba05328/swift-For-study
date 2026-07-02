522b719f2a059cb93d632e1bcf2ba04ec331fc8f
haruki328 <y.haruki328@gmail.com>
2026-07-02 09:23:24 +0900

    Day1: setup Xcode project

---
+++ SwiftForStudy/SwiftForStudy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftForStudy
+//
+//  Created by 山本悠生 on 2026/07/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("こんにちは、Swift！")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
